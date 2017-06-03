181f05deb70d0e1d466b407392ad593cf55e248b
Izzet Fatullayev <izzetfatullayev93@gmail.com>
2017-06-03 13:39:36 +0400

    Admin lte installed

--- common/config/main.php
+++ common/config/main.php
@@ -5,9 +5,15 @@ return [
         '@bower' => '@vendor/bower-asset',
         '@npm' => '@vendor/npm-asset',
     ],
+    'bootstrap' => [
+        'common\bootstrap\SetUp',
+    ],
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
+            'cachePath' => '@common/runtime/cache',
+            //'class' => 'yii\caching\MemCache',
+            //'useMemcached' => true,
         ],
     ],
 ];
