704fe8ceab487affe6edfbeb744346665dfb4f9f
Izzet Fatullayev <izzetfatullayev93@gmail.com>
2017-08-22 12:45:56 +0400

    End of 3rd lesson (added reviews to SaveRelationsBehavior)

--- shop/entities/Shop/Product/Product.php
+++ shop/entities/Shop/Product/Product.php
@@ -48,7 +48,7 @@ class Product extends ActiveRecord
             MetaBehavior::class,
             [
                 'class' => SaveRelationsBehavior::class,
-                'relations' => ['categoryAssignments', 'tagAssignments', 'values', 'photos', 'relatedProductAssignments', 'modifications'],
+                'relations' => ['categoryAssignments', 'tagAssignments', 'values', 'photos', 'relatedProductAssignments', 'modifications', 'reviews'],
             ],
         ];
     }
